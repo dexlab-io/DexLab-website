c06196a419ef8f0996dad62e9bd4ccc392e08be8
Parminder Klair <perminder.klair@gmail.com>
2019-04-30 11:41:33 +0530

    added v2 non react website

--- src/utils/lang.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  heroTitle: `The ultimate one-stop fintech hub for everyone.`,
-  heroSubTitle: `Discover a revolutionary ecosystem of decentralized tools and services with people at its core.`,
-  subscribeText: `Subscribe to our newsletter and be the first to know.`,
-  newsletterText: `I want to receive the newsletter`,
-};
